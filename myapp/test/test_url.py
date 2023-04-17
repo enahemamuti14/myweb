839d041a2fd2b7d9439fefcae7512afa2711fcb8
enahemamuti14 <enahemamuti@gmail.com>
2023-04-17 10:55:40 +0700

    file baru

--- myapp/test/test_url.py
+++ myapp/test/test_url.py
@@ -4,7 +4,18 @@ from myapp.views import index, send_contact,send_order, product
 
 class TestUrls(SimpleTestCase):
 
-    def test_urls(self):
+    def test_index_url_is_resolved(self):
         url = reverse('index')
-        print(resolve(url))
         self.assertEquals(resolve(url).func, index)
+
+    def test_send_contact_url_is_resolved(self):
+        url = reverse('send_contact')
+        self.assertEquals(resolve(url).func, send_contact)
+
+    def test_send_order_url_is_resolved(self):
+        url = reverse('send_order')
+        self.assertEquals(resolve(url).func, send_order)
+
+    def test_product_url_is_resolved(self):
+        url = reverse('product')
+        self.assertEquals(resolve(url).func, product)
