e7d2b3a644f3d9098cda0d1282f3c9c6a55a7437
enahemamuti14 <enahemamuti@gmail.com>
2023-04-19 14:39:11 +0700

    file baru

--- myapp/models.py
+++ myapp/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+import uuid
 
 class Contact(models.Model):
     nama = models.CharField(max_length = 30)
     email = models.EmailField(max_length = 30)
     pesan = models.TextField()
-    tanggal = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.nama
     
 class order(models.Model):
+    id = models.AutoField(primary_key=True)
     nama = models.CharField(max_length= 30)
     email = models.EmailField(max_length = 30)
     alamat = models.CharField(max_length= 100)
